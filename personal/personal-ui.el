d3bc8c47c150ca3087d0f818d58c0b5258ddf7ba
Margarita Karkali <margarita@skroutz.gr>
2018-05-28 13:20:22 +0300

    Update my personal settings

--- personal/personal-ui.el
+++ personal/personal-ui.el
@@ -8,6 +8,18 @@
 (scroll-bar-mode -1)
 
 ;; smooth mouse scrolling
-(setq mouse-wheel-scroll-amount '(1 ((shift) . 5))
-      mouse-wheel-progressive-speed) nil
+(setq mouse-wheel-scroll-amount '(1 ((shift) . 5)))
+
+(setq mouse-wheel-progressive-speed nil)
+
+(add-to-list 'auto-mode-alist '("\\.hbs\\'" . web-mode))
+
+(require 'multiple-cursors)
+
+(global-set-key (kbd "C-c m c") 'mc/edit-lines)
+
+(global-set-key (kbd "C->") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
+
 ;;; personal-ui.el ends here.
